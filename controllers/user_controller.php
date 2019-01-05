69dbbdb5a8a77e3957e094c38dd79a47bb40cb4a
AJGoh <46371098+AJGoh@users.noreply.github.com>
2019-01-05 10:23:54 +0800

    jiahui version

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -102,10 +102,10 @@ class user_controller extends CI_Controller {
 
         $this->load->library('email');
 
-        $subject = 'Panda Ticketing System Activation Email';
+        $result = $this->db->query("SELECT * FROM ost_user_test WHERE user_email = '$user_email'");
 
         $data = array(
-            'result' => $this->db->query("SELECT * FROM ost_user_test WHERE user_email = '$user_email'"),
+            'body' => $this->db->query("SELECT REPLACE(REPLACE(REPLACE(body, '%user_name%', '".$result->row('user_name')."'), 'activateuserguest', 'activateuser'), '%user_id%', '".$result->row('user_id')."') AS email, subject FROM ost_email_template_test WHERE id = '22'"),
             'template' => $this->db->query("SELECT * FROM ost_company_test"),
         );
 
@@ -120,14 +120,14 @@ class user_controller extends CI_Controller {
             'smtp_port' => $sender_email->smtp_port,
                             
         );
-        $bodyContent = $this->load->view('activateemail', $data, TRUE);
+        $bodyContent = $this->load->view('email_template', $data, TRUE);
         
         $result = $this->email
             ->initialize($config)
             ->from($sender_email->userid)
             ->reply_to($sender_email->userid)    // Optional, an account where a human being reads.
             ->to($user_email)
-            ->subject($subject)
+            ->subject($data['body']->row('subject'))
             ->message($bodyContent)
             ->send();
 
@@ -153,18 +153,16 @@ class user_controller extends CI_Controller {
         echo "<script>
                 document.location='" . base_url() . "/index.php/user_controller/login'
               </script>";
-
     }
 
     public function activateuserguest()
     {
         $user_id = $_REQUEST['id'];
         $data = array(
-            'result' => $this->db->query("SELECT * FROM ost_user_test WHERE user_id = '$user_id'")        
+            'result' => $this->db->query("SELECT * FROM ost_user_test WHERE user_id = '$user_id'")
         );
 
-                $this->load->view('user/guestuseractivate', $data); 
-
+        $this->load->view('user/guestuseractivate', $data); 
     }
 
     public function activateuserguestconfirm()
