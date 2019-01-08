64e9cb53edef38923572781b429e75201a0571ad
AJGoh <46371098+AJGoh@users.noreply.github.com>
2019-01-08 14:18:05 +0800

    Add files via upload

--- views/admin_emails/admin_emails_templates.php
+++ views/admin_emails/admin_emails_templates.php
@@ -1,6 +1,5 @@
 <div id="content">
-
-        <form action="<?php echo site_url('admin_emails_controller/emails_banlist_process')?>" method="POST" name="roles">
+    <form action="<?php echo site_url('admin_emails_controller/emails_templates_process')?>" method="POST" name="roles">
 <div class="sticky placeholder"></div><div class="sticky bar">
     <div class="content" style="width: 908px;">
         <div class="pull-left">
@@ -8,116 +7,104 @@
             </h2>
         </div>
         <div class="pull-right flush-right">
-            <a href="<?php echo site_url('admin_emails_controller/emails_banlist_add')?>" class="green button action-button"><i class="icon-plus-sign"></i> Add New Template Set</a>
+            <a href="<?php echo site_url('admin_emails_controller/emails_templates_add')?>" class="green button action-button"><i class="icon-plus-sign"></i> Add New Template Set</a>
             <span class="action-button" data-dropdown="#action-dropdown-more">
                 <i class="icon-caret-down pull-right"></i>
                 <span><i class="icon-cog"></i> More</span>
             </span>
             <div id="action-dropdown-more" class="action-dropdown anchor-right" style="display: none;">
                 <ul>
-                        <li>
-                            <a id="enable" class="confirm" data-toggle="modal" data-target="#more-modal" href="#?title=enable">
-                                <i class="icon-ok-sign icon-fixed-width"></i>
-                                Enable                            </a>
-                        </li>
-                        <li>
-                            <a id="disable" class="confirm" data-toggle="modal" data-target="#more-modal" href="#?title=disable">
-                                <i class="icon-ban-circle icon-fixed-width"></i>
-                                Disable                            </a>
-                        </li>
-                        <li class="danger">
-                            <a id="delete" class="confirm" data-toggle="modal" data-target="#more-modal" href="#?title=delete">
-                                <i class="icon-trash icon-fixed-width"></i>
-                                Delete                            </a>
-                        </li>
-                    </ul>
+                    <li>
+                        <a id="enable" class="confirm" data-toggle="modal" data-target="#more-modal" href="#?title=enable">
+                            <i class="icon-ok-sign icon-fixed-width"></i>
+                            Enable</a>
+                    </li>
+                    <li>
+                        <a id="disable" class="confirm" data-toggle="modal" data-target="#more-modal" href="#?title=disable">
+                            <i class="icon-ban-circle icon-fixed-width"></i>
+                            Disable</a>
+                    </li>
+                    <li class="danger">
+                        <a id="delete" class="confirm" data-toggle="modal" data-target="#more-modal" href="#?title=delete">
+                            <i class="icon-trash icon-fixed-width"></i>
+                            Delete</a>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="clear"></div>
     <a class="only sticky scroll-up" href="#" data-stop="135"><i class="icon-chevron-up icon-large"></i></a></div>
 </div>
 
- <div style="overflow:auto;">
-
+<div style="overflow:auto;">
 <table id="ttable" class="list" border="0" cellspacing="1" cellpadding="0" width="100%" style="white-space: nowrap;">
-        <thead>
-            <tr>
+    <thead>
+        <tr>
             <th nowrap="" width="4%" style="text-align: center;"><input type="checkbox" id="tidsall" onclick="checkedAll ();"></th>
             <th style="padding-left:4px;vertical-align:middle" width="30%">Name</th>
             <th style="padding-left:4px;vertical-align:middle" width="8%">Status</th>
-          
             <th style="padding-left:4px;vertical-align:middle" width="8%">In-use</th>
             <th style="padding-left:4px;vertical-align:middle" width="8%">Date Added</th>
             <th style="padding-left:4px;vertical-align:middle" width="8%">Last Updated</th>
-
         </tr>
-        </thead>
-        <tbody><?php foreach ($templategroup->result() as $value) { ?>
-                    <tr>
-                    <td align="center">
-                      <input type="checkbox" class="ckb" id="tids" name="tids[]" value="<?php echo $value->tpl_id?>">
-                    </td>
-                    <td>&nbsp;<a href="<?php echo site_url('admin_emails_controller/emails_template_info')?>?id=<?php echo $value->tpl_id?>"><?php echo $value->name?></a></td>
-                    <td><?php if ($value->isactive == 1){ ?>
-
-                      Active
-
-                      <?php } else if ($value->isactive == 0) { ?>
-
-                      Inactive
-
-                      <?php } ?></td>
-                    <td>Need to find where is it in database</td>
-                    <td><?php echo $value->created?></td>
-                    <td><?php echo $value->updated?></td>
-                   </tr>
-                 <?php } ?>
-                        </tbody><tfoot>
-
-        </tfoot>
-    </table>
-
+    </thead>
+    
+    <tbody>
+        <?php foreach ($templategroup->result() as $value) { ?>
+            <tr>
+                <td align="center"><input type="checkbox" class="ckb" id="tids" name="tids[]" value="<?php echo $value->tpl_id?>" <?php echo $default_template_id->row('value') == $value->tpl_id?"disabled":"";?>></td>
+                <td>&nbsp;<a href="<?php echo site_url('admin_emails_controller/emails_templates_info')?>?id=<?php echo $value->tpl_id?>"><?php echo $value->name?></a> <?php echo $default_template_id->row('value') == $value->tpl_id?" <small>(System Default)</small>":"";?></td>
+                <td><?php if ($value->isactive == 1){ ?>
+                        Active
+                    <?php } else if ($value->isactive == 0) { ?>
+                        Inactive
+                    <?php } ?></td>
+                <td><?php echo $default_template_id->row('value') == $value->tpl_id?"Yes":"No";?></td>
+                <td><?php echo $value->created?></td>
+                <td><?php echo $value->updated?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
 </div>
 
 <!-- more popup modal -->
 <div class="modal fade" id="more-modal" style="display: none;">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">×</span></button>
+                <span aria-hidden="true">×</span></button>
                 <h3>Please Confirm</h3>
-              </div>
-              <div class="modal-body">
-
-               <p class="confirm-action" style="display: block;" id="enable-confirm">
-                Are you sure you want to <b>enable</b> selected team(s)?    </p>
-
-              <p class="confirm-action" style="display: none;" id="disable-confirm">
-                Are you sure you want to <b>disable</b> selected team(s)?     </p>
-
-              <p class="confirm-action" style="display: none;" id="delete-confirm">
-                <font color="red"><strong>Are you sure you want to DELETE selected team(s)? </strong></font>
-              </p>
-
-              <div>Please confirm to continue.</div>
-
-              </div>
-              <div class="modal-footer">
-              <p class="full-width">
-                <span class="buttons pull-left">
-                    <input type="button" value="No, Cancel" class="close">
-                </span>
-                <span class="buttons pull-right">
-                    <input type="submit" value="Yes, Do it!" class="confirm">
-                </span>
-              </p>
-              </div>
             </div>
-            <!-- /.modal-content -->
-          </div>
-          <!-- /.modal-dialog -->
-          <input type="hidden" name="status">
+            <div class="modal-body">
+                <p class="confirm-action" style="display: block;" id="enable-confirm">
+                Are you sure you want to <b>enable</b> selected team(s)?</p>
+
+                <p class="confirm-action" style="display: none;" id="disable-confirm">
+                Are you sure you want to <b>disable</b> selected team(s)?</p>
+
+                <p class="confirm-action" style="display: none;" id="delete-confirm">
+                    <font color="red"><strong>Are you sure you want to DELETE selected team(s)? </strong></font>
+                </p>
+            <div>Please confirm to continue.</div>
+            </div>
+
+            <div class="modal-footer">
+                <p class="full-width">
+                    <span class="buttons pull-left">
+                        <input type="button" value="No, Cancel" class="close">
+                    </span>
+                    <span class="buttons pull-right">
+                        <input type="submit" value="Yes, Do it!" class="confirm">
+                    </span>
+                </p>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+    <input type="hidden" name="status">
 </div>
 <!-- more popup modal -->
 
@@ -139,9 +126,7 @@ function checkedAll () {
     }
  }
 
-
 </script>
-
 
 <script>
   $(document).ready(function () {    
@@ -163,32 +148,29 @@ function checkedAll () {
 <script type="text/javascript">
 $(document).ready(function () {
     $('#enable').click(function() {
-      checked = $("input[id=tids]:checked").length;
+        checked = $("input[id=tids]:checked").length;
 
-      if(!checked) {
-        alert("You must check at least one ticket.");
-        return false;
-      }
-      document.getElementById("disable-confirm").style.display = "none";
-      document.getElementById("delete-confirm").style.display = "none";
-      document.getElementById("enable-confirm").style.display = "block";
-      $('#more-modal').find("[name=status]").val(1);
-      
-
+        if(!checked) {
+          alert("You must check at least one ticket.");
+          return false;
+        }
+        document.getElementById("disable-confirm").style.display = "none";
+        document.getElementById("delete-confirm").style.display = "none";
+        document.getElementById("enable-confirm").style.display = "block";
+        $('#more-modal').find("[name=status]").val(1);
     });
 
     $('#disable').click(function() {
-      checked = $("input[id=tids]:checked").length;
+        checked = $("input[id=tids]:checked").length;
 
-      if(!checked) {
-        alert("You must check at least one ticket.");
-        return false;
-      }
-      document.getElementById("enable-confirm").style.display = "none";
-      document.getElementById("delete-confirm").style.display = "none";
-      document.getElementById("disable-confirm").style.display = "block";
-      $('#more-modal').find("[name=status]").val(0);
-
+        if(!checked) {
+          alert("You must check at least one ticket.");
+          return false;
+        }
+        document.getElementById("enable-confirm").style.display = "none";
+        document.getElementById("delete-confirm").style.display = "none";
+        document.getElementById("disable-confirm").style.display = "block";
+        $('#more-modal').find("[name=status]").val(0);
     });
 
     $('#delete').click(function() {
@@ -202,11 +184,7 @@ $(document).ready(function () {
       document.getElementById("disable-confirm").style.display = "none";
       document.getElementById("delete-confirm").style.display = "block";
       $('#more-modal').find("[name=status]").val(2);
-
     });
-
-    
-
 });
 
 </script>
