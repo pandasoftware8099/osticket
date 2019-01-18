0967d519ef119ee3b5dea00c961492ee1acb885a
realjimjim <46802228+realjimjim@users.noreply.github.com>
2019-01-18 17:55:13 +0800

    enable_kb and search kb and featurelogic

--- views/welcome_message.php
+++ views/welcome_message.php
@@ -3,11 +3,20 @@
          <div id="landing_page">
 <div class="col-lg-8">
 <div class="search-form">
-    <form method="get" action="kb/faq.php">
-    <input type="hidden" name="a" value="search">
-    <input type="text" name="q" class="search" placeholder="Search our knowledge base">
+
+
+    <?php if ($enable_kb == '1') {  ?>
+
+    <form method="post" action="<?php echo site_url('guide_controller/faqsearch_process');?>">
+    <input type="text" name="search" class="search" placeholder="Search our knowledge base">
     <button type="submit" class="green button">Search</button>
+
     </form>
+
+    
+    <?php } ?>
+
+
 </div>
     <div class="thread-body">
         <?php echo $landpages->row('body');?> 
@@ -16,25 +25,65 @@
 <div class="col-lg-4">
     <div>
         <div class="front-page-button flush-right">
-<p>
-            <a href="<?php echo site_url('open_controller/main')?>" style="display:block" class="blue button">Open a New Ticket</a>
-</p>
-<p>
-            <a href="<?php echo site_url('ticket_controller/main')?>" style="display:block" class="green button">Check Ticket Status</a>
-</p>
+            <p>
+                <a href="<?php echo site_url('open_controller/main')?>" style="display:block" class="blue button">Open a New Ticket</a>
+            </p>
+            <p>
+                <a href="<?php echo site_url('ticket_controller/main')?>" style="display:block" class="green button">Check Ticket Status</a>
+            </p>
         </div>
-        <div class="content">            <section><div class="header">Featured Questions</div>
-            <div><a href="/helpdesk/kb/faq.php?id=3">How to register client account ?</a></div>
-            <div><a href="/helpdesk/kb/faq.php?id=4">How to create new ticket ?</a></div>
-            <div><a href="/helpdesk/kb/faq.php?id=5">How to reset password ?</a></div>
-            <div><a href="/helpdesk/kb/faq.php?id=8">Cannot create new ticket ?</a></div>
-            <div><a href="/helpdesk/kb/faq.php?id=6">How to login as agent ?</a></div>
+
+<?php if ($enable_kb == '1') {  ?>
+        <div class="content">            
+            <section>
+            <div class="header">Featured Questions</div>
+            <?php foreach ($feature_question->result() as $value ) { ?>
+                <div><a href="<?php echo site_url('guide_controller/info?id=');echo $value->faq_guid ?>"><?php echo $value->question ?> ?</a></div>
+            <?php } ?>
             </section>
-</div>
+        </div>
+<?php } ?>
+
     </div>
 
 </div>
 <div class="clear"></div>
+
+<?php if ($enable_kb == '1') {  ?>
+
+<div>
+<br><br>
+
+<?php if($feature_kb->num_rows() > '0'  ) { ?>
+
+<h1>Featured Knowledge Base Articles</h1>
+
+<?php } ?>
+
+<?php foreach ($feature_kb->result() as $value) { ?>
+
+<div class="featured-category front-page">
+        <i class="icon-folder-open icon-2x"></i>
+        <div class="category-name"><?php echo $value->name ?></div>
+
+        <?php foreach ( $this->db->query("SELECT * FROM ost_faq_test WHERE ispublished = '2' AND category_guid = '$value->category_guid' ")->result() as $faq) { ?>
+
+        <div class="article-headline">
+            <div class="article-title"><a href="<?php echo site_url('guide_controller/info?id=');echo $faq->faq_guid ?>"><?php echo $faq->question ?></a></div>
+            <div class="article-teaser"><?php echo strip_tags($faq->answer) ?></div>
+        </div>
+
+        <?php } ?>
+        
+    </div>
+
+
+
+<?php } ?>
+    
+</div>
+
+<?php }  ?>
 
 <div>
 <br><br>
