be040db3b62bb5b7db17776cb393d31fe442f961
realjimjim <46802228+realjimjim@users.noreply.github.com>
2019-01-23 16:27:41 +0800

    Add files via upload

---
+++ views/footerstaff.php
@@ -0,0 +1,59 @@
+
+<div>
+    <div id="footer" style="margin-bottom:10px;">
+        Copyright © 2006-2018&nbsp;Panda Ticketing System&nbsp;All Rights Reserved.
+    </div>
+    <div>
+        <!-- Do not remove <img src="autocron.php" alt="" width="1" height="1" border="0" /> or your auto cron will cease to function -->
+        <!-- <img src="/helpdesk/scp/autocron.php" alt="" width="1" height="1" border="0"> -->
+        <!-- Do not remove <img src="autocron.php" alt="" width="1" height="1" border="0" /> or your auto cron will cease to function -->
+    </div>
+</div>
+
+<div id="loading" style="top: 219px; left: 533px;">
+    <i class="icon-spinner icon-spin icon-3x pull-left icon-light"></i>
+    <h1>Loading ...</h1>
+</div>
+<!-- <div class="container dialog draggable ui-draggable size-large" id="popup">
+    <div id="popup-loading" style="display: none;">
+        <h1 style="margin-bottom: 20px; margin-top: 6px;"><i class="icon-spinner icon-spin icon-large"></i>
+        Loading ...</h1>
+    </div>
+
+</div> -->
+<div style="display: none; top: 54.75px; left: 627px;" class="dialog" id="alert">
+    <h3><i class="icon-warning-sign"></i> <span id="title"></span></h3>
+    <a class="close" href=""><i class="icon-remove-circle"></i></a>
+    <hr>
+    <div id="body" style="min-height: 20px;"></div>
+    <hr style="margin-top:3em">
+    <p class="full-width">
+        <span class="buttons pull-right">
+            <input type="button" value="OK" class="close ok">
+        </span>
+     </p>
+    <div class="clear"></div>
+</div>
+
+
+<!-- <script type="text/javascript" src="/helpdesk/js/jquery.pjax.js?9ae093d"></script> -->
+<script type="text/javascript" src="/helpdesk/scp/js/bootstrap-typeahead.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/scp/js/scp.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/js/jquery-ui-1.10.3.custom.min.js?9ae093d"></script>
+<!-- <script type="text/javascript" src="/helpdesk/js/filedrop.field.js?9ae093d"></script> -->
+<script type="text/javascript" src="/helpdesk/js/select2.min.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/scp/js/tips.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/js/redactor.min.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/js/redactor-osticket.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/js/redactor-plugins.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/scp/js/jquery.translatable.js?9ae093d"></script>
+<!-- <script type="text/javascript" src="/helpdesk/scp/js/jquery.dropdown.js?9ae093d"></script> -->
+<script type="text/javascript" src="/helpdesk/scp/js/bootstrap-tooltip.js?9ae093d"></script>
+<script type="text/javascript" src="/helpdesk/js/fabric.min.js?9ae093d"></script>
+<link type="text/css" rel="stylesheet" href="/helpdesk/scp/css/tooltip.css?9ae093d">
+<script type="text/javascript">
+    getConfig().resolve({"lock_time":3600,"html_thread":true,"date_format":"Y-MM-dd","lang":"en_US","short_lang":"en","has_rtl":false,"lang_flag":"us","primary_lang_flag":"us","primary_language":"en-US","secondary_languages":[],"page_size":25});
+</script>
+
+
+<ul class="typeahead dropdown-menu" style="display: none;"></ul><div id="ui-datepicker-div" class="ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all"></div><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-html" style="display: none;">HTML</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-formatting" style="display: none;">Formatting</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-bold" style="display: none;">Bold</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-italic" style="display: none;">Italic</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-underline" style="display: none;">Underline</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-deleted" style="display: none;">Deleted</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-unorderedlist" style="display: none;">• Unordered List</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-orderedlist" style="display: none;">1. Ordered List</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-outdent" style="display: none;">&lt; Outdent</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-indent" style="display: none;">&gt; Indent</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-image" style="display: none;">Insert Image</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-link" style="display: none;">Link</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-alignment" style="display: none;">Alignment</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-horizontalrule" style="display: none;">Insert Horizontal Rule</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-table" style="display: none;">Table</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-video" style="display: none;">Insert Video</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-fontcolor" style="display: none;">Font Color</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-backcolor" style="display: none;">Back Color</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-fontfamily" style="display: none;">Change Font Family</span><span class="redactor-toolbar-tooltip redactor-toolbar-tooltip-1 redactor-toolbar-tooltip-deleteDraft" style="display: none;">Delete Draft</span></body></html>
