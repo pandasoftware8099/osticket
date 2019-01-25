118b82eb5594cd854e74824e52c963be055aab69
realjimjim <46802228+realjimjim@users.noreply.github.com>
2019-01-25 11:01:01 +0800

    graph and bar

--- views/staff/staff_dashboard.php
+++ views/staff/staff_dashboard.php
@@ -1,12 +1,5 @@
 <div id="content">
-        <script type="text/javascript" src="js/raphael-min.js?9ae093d"></script>
-<script type="text/javascript" src="js/g.raphael.js?9ae093d"></script>
-<script type="text/javascript" src="js/g.line-min.js?9ae093d"></script>
-<script type="text/javascript" src="js/g.dot-min.js?9ae093d"></script>
-<script type="text/javascript" src="js/dashboard.inc.js?9ae093d"></script>
-
-<link rel="stylesheet" type="text/css" href="css/dashboard.css?9ae093d">
-
+<script src="<?php echo base_url('asset/dist/js/Chart.js');?>"></script>
 <form method="post" action="dashboard.php">
 <div id="basic_search">
     <div style="min-height:25px;">
@@ -42,11 +35,120 @@
     </div>
 </div>
 <div class="clear"></div>
-<!-- Create a graph and fetch some data to create pretty dashboard -->
-<div style="position:relative">
-    <div id="line-chart-here" style="height:300px"><svg height="300" version="1.1" width="908" xmlns="http://www.w3.org/2000/svg" style="overflow: hidden; position: relative; left: -0.890625px; top: -0.109375px;"><desc style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Created with Raphaël 2.1.0</desc><defs style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></defs><text x="40" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-18-2018</tspan></text><text x="137.65078" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-20-2018</tspan></text><text x="235.30156" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-22-2018</tspan></text><text x="332.95234" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-23-2018</tspan></text><text x="430.60312" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-25-2018</tspan></text><text x="528.2538999999999" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-27-2018</tspan></text><text x="625.9046799999999" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-29-2018</tspan></text><text x="723.5554599999998" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">10-30-2018</tspan></text><text x="821.2062399999998" y="275.20624" text-anchor="middle" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">11-01-2018</tspan></text><path fill="none" stroke="#000000" d="M40,263.70624L821.20624,263.70624M40.5,263.20624L40.5,268.20624M138.15078,263.20624L138.15078,268.20624M235.80156,263.20624L235.80156,268.20624M333.45234,263.20624L333.45234,268.20624M431.10312,263.20624L431.10312,268.20624M528.7538999999999,263.20624L528.7538999999999,268.20624M626.4046799999999,263.20624L626.4046799999999,268.20624M724.0554599999998,263.20624L724.0554599999998,268.20624M821.7062399999998,263.20624L821.7062399999998,268.20624" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path><text x="35" y="263.20624" text-anchor="end" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5031149999999798" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">0</tspan></text><text x="35" y="141.60312" text-anchor="end" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.50936999999999" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">1</tspan></text><text x="35" y="20" text-anchor="end" font="11px 'Fontin Sans', Fontin-Sans, sans-serif" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font: 11px &quot;Fontin Sans&quot;, Fontin-Sans, sans-serif;"><tspan dy="3.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">2</tspan></text><path fill="none" stroke="#000000" d="M40.5,263.20624L40.5,20M36,263.70624L41,263.70624M36,142.10312L41,142.10312M36,20.5L41,20.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path><path fill="none" stroke="#2f69bf" d="M40,20L319.0022285714286,141.60312L430.60312,141.60312L542.2040114285714,263.20624L653.8049028571429,20L709.6053485714285,141.60312L765.4057942857143,141.60312" stroke-width="1.6" stroke-linejoin="round" stroke-linecap="round" stroke-dasharray="-1.6" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-linejoin: round; stroke-linecap: round;"></path><circle cx="40" cy="20" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="319.0022285714286" cy="141.60312" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="430.60312" cy="141.60312" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="542.2040114285714" cy="263.20624" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="653.8049028571429" cy="20" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="709.6053485714285" cy="141.60312" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="765.4057942857143" cy="141.60312" r="4.800000000000001" fill="#2f69bf" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><path fill="none" stroke="#a2bf2f" d="M40,263.20624L319.0022285714286,263.20624L430.60312,141.60312L542.2040114285714,263.20624L653.8049028571429,263.20624L709.6053485714285,263.20624L765.4057942857143,263.20624" stroke-width="1.6" stroke-linejoin="round" stroke-linecap="round" stroke-dasharray="-1.6" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-linejoin: round; stroke-linecap: round;"></path><circle cx="40" cy="263.20624" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="319.0022285714286" cy="263.20624" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="430.60312" cy="141.60312" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="542.2040114285714" cy="263.20624" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="653.8049028571429" cy="263.20624" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="709.6053485714285" cy="263.20624" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="765.4057942857143" cy="263.20624" r="4.800000000000001" fill="#a2bf2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><path fill="none" stroke="#bf5a2f" d="M40,141.60312L319.0022285714286,263.20624L430.60312,263.20624L542.2040114285714,141.60312L653.8049028571429,263.20624L709.6053485714285,263.20624L765.4057942857143,263.20624" stroke-width="1.6" stroke-linejoin="round" stroke-linecap="round" stroke-dasharray="-1.6" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-linejoin: round; stroke-linecap: round;"></path><circle cx="40" cy="141.60312" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="319.0022285714286" cy="263.20624" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="430.60312" cy="263.20624" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="542.2040114285714" cy="141.60312" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="653.8049028571429" cy="263.20624" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="709.6053485714285" cy="263.20624" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="765.4057942857143" cy="263.20624" r="4.800000000000001" fill="#bf5a2f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><path fill="none" stroke="#bfa22f" d="M40,263.20624L319.0022285714286,141.60312L430.60312,141.60312L542.2040114285714,263.20624L653.8049028571429,20L709.6053485714285,263.20624L765.4057942857143,20" stroke-width="1.6" stroke-linejoin="round" stroke-linecap="round" stroke-dasharray="-1.6" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-linejoin: round; stroke-linecap: round;"></path><circle cx="40" cy="263.20624" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="319.0022285714286" cy="141.60312" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="430.60312" cy="141.60312" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="542.2040114285714" cy="263.20624" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="653.8049028571429" cy="20" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="709.6053485714285" cy="263.20624" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><circle cx="765.4057942857143" cy="20" r="4.800000000000001" fill="#bfa22f" stroke="none" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></circle><rect x="29" y="0" width="150.5011142857143" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect><rect x="178.5011142857143" y="0" width="196.30156" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect><rect x="373.80267428571426" y="0" width="112.60089142857143" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect><rect x="485.4035657142857" y="0" width="112.60089142857146" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect><rect x="597.0044571428572" y="0" width="84.70066857142854" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect><rect x="680.7051257142857" y="0" width="56.80044571428567" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect><rect x="736.5055714285713" y="0" width="66.80044571428573" height="283.20624" r="0" rx="0" ry="0" fill="#000000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect></svg></div>
-    <div style="position:absolute;right:0;top:0" id="line-chart-legend"><span class="label" style="margin-left: 0.5em; background-color: rgb(47, 105, 191);">created</span><br><span class="label" style="margin-left: 0.5em; background-color: rgb(162, 191, 47);">reopened</span><br><span class="label" style="margin-left: 0.5em; background-color: rgb(191, 90, 47);">assigned</span><br><span class="label" style="margin-left: 0.5em; background-color: rgb(191, 162, 47);">overdue</span><br></div>
-</div>
+
+
+
+<canvas id="linechart" width="400" height="100"></canvas>
+<script>
+var canvas = document.getElementById("linechart");
+var ctx = canvas.getContext('2d');
+
+// Global Options:
+Chart.defaults.global.defaultFontColor = 'black';
+Chart.defaults.global.defaultFontSize = 16;
+
+var data = {
+  labels: [<?php for ($i = 1; $i <= 12; $i++) {
+    $months = date("M", strtotime( date( 'Y-m-01' )." +$i months"));
+    echo " ' ".$months." ' ,";
+} ?>],
+  datasets: [{
+      label: "Created",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(225,0,0,0.4)",
+      borderColor: "red", // The main line color
+      borderCapStyle: 'square',
+      borderDash: [], // try [5, 15] for instance
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "black",
+      pointBackgroundColor: "white",
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "yellow",
+      pointHoverBorderColor: "brown",
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: true
+      data: [<?php foreach ($ticketcreated_graph->result() as $value) {
+    foreach ($value as $value) {
+
+    echo $value.',';
+}
+} ?>],
+      
+      
+      spanGaps: true,
+    }
+
+  ]
+};
+
+// Notice the scaleLabel at the same level as Ticks
+var options = {
+  scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                },
+                scaleLabel: {
+                     display: true,
+                     labelString: 'Ticket',
+                     fontSize: 20 
+                  }
+            }]            
+        }  
+};
+
+// Chart declaration:
+var myBarChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: options
+});
+</script>
+
+<canvas id="myChart" width="400" height="100"></canvas>
+<script>
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Open", "Closed", "Resolved", "Overdue", "Assigned"],
+        datasets: [{
+            label: 'Tickets',
+            data: [<?php echo $open ?>, <?php echo $closed ?>, <?php echo $resolved ?>, <?php echo $overdue ?>, <?php echo $assign ?>],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+
 
 <hr>
 <h2>Statistics&nbsp;<i class="help-tip icon-question-sign" href="#statistics"></i></h2>
@@ -202,7 +304,7 @@
 </form>
 
 <script>
-    $.drawPlots({"times":[1539820800,1540252800,1540425600,1540598400,1540771200,1540857600,1540944000],"plots":{"created":[2,1,1,0,2,1,1],"reopened":[0,0,1,0,0,0,0],"assigned":[1,0,0,1,0,0,0],"overdue":[0,1,1,0,2,0,2]},"events":["created","reopened","assigned","overdue"]});
+
 </script>
 </div>
 </div>
