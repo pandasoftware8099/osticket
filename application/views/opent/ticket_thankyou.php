d3c082305e9eec395258c33641fc0147c6c3a682
realjimjim <46802228+realjimjim@users.noreply.github.com>
2019-02-01 11:20:28 +0800

    remove each page footer

--- application/views/opent/ticket_thankyou.php
+++ application/views/opent/ticket_thankyou.php
@@ -5,14 +5,3 @@
     </div>
 </div>
 
-<div id="footer">
-    <p>Copyright © 2018 Panda Ticketing System - All rights reserved.</p>
-    <a id="poweredBy" href="http://osticket.com" target="_blank">Helpdesk software - powered by osTicket</a>
-</div>
-
-<script type="text/javascript">
-    getConfig().resolve({"html_thread":true,"lang":"en_US","short_lang":"en","has_rtl":false,"primary_language":"en-US","secondary_languages":[]});
-</script>
-
-</body>
-</html>
