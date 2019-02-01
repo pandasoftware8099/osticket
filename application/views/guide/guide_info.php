91d8224db3dcc872d2a7cc75c6ccefb08c7fdfca
realjimjim <46802228+realjimjim@users.noreply.github.com>
2019-02-01 11:22:07 +0800

    remove each page footer

--- application/views/guide/guide_info.php
+++ application/views/guide/guide_info.php
@@ -72,18 +72,3 @@ style=" background-color: rgb(235, 234, 234);
 </div>
 
     </div>
-    <div id="footer">
-        <p>Copyright © 2018 Panda Ticketing System - All rights reserved.</p>
-        <a id="poweredBy" href="http://osticket.com" target="_blank">Helpdesk software - powered by osTicket</a>
-    </div>
-<div id="overlay" style="opacity: 0.3; top: 0px; left: 0px;"></div>
-<div id="loading" style="top: 219px; left: 514.5px;">
-    <h4>Please Wait!</h4>
-    <p>Please wait... it will take a second!</p>
-</div>
-<script type="text/javascript">
-    getConfig().resolve({"html_thread":true,"lang":"en_US","short_lang":"en","has_rtl":false,"primary_language":"en-US","secondary_languages":[]});
-</script>
-
-
-</body></html>
