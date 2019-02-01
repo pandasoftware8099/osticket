05cb93fc36020f79fa2da7d7eccf1cbb51a59fac
realjimjim <46802228+realjimjim@users.noreply.github.com>
2019-02-01 11:23:07 +0800

    remove each page footer

--- application/views/viewt/ticket_info.php
+++ application/views/viewt/ticket_info.php
@@ -137,9 +137,11 @@ $.dialog(url, [201], function(xhr, resp) {
                     <i class="icon-copy"></i> View History</a></li>
             </ul>
         </div>
+         <?php if($value1->editor != ''){ ?>
         <span class="textra light">
             <span class="label label-bare" title="Edited on  by Hugh Panda">Edited</span>
         </span>
+        <?php } ?>
         </div>
                             <?php if ($value1->staff_guid == '0') { ?>
                                 <b><?php echo $value1->poster;?></b>
@@ -261,19 +263,8 @@ echo $file;
 <link rel="stylesheet" type="text/css" href="/helpdesk/css/filedrop.css">
 </div>
 </div>
+</div>
 
-<div id="footer">
-    <p>Copyright © 2018 Panda Ticketing System - All rights reserved.</p>
-    <a id="poweredBy" href="http://osticket.com" target="_blank">Helpdesk software - powered by osTicket</a>
-</div>
-<div id="overlay" style="opacity: 0.3; top: 0px; left: 0px;"></div>
-<div id="loading" style="top: 219px; left: 514.5px;">
-    <h4>Please Wait!</h4>
-    <p>Please wait... it will take a second!</p>
-</div>
-<script type="text/javascript">
-    getConfig().resolve({"html_thread":true,"lang":"en_US","short_lang":"en","has_rtl":false,"primary_language":"en-US","secondary_languages":[]});
-</script>
 
 <script>
 function reload() {
@@ -310,4 +301,3 @@ $(document).on('change', '.file', function(){
     }
 });
 </script>
-</body></html>
